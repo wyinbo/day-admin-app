1ef81e2a833e1a30ccc9f21959dc3124da7aa6c3
wyinbo <wuyinbo@126.com>
2021-06-20 12:41:54 +0800

    每日优鲜后台初始布局

---
+++ src/views/layout/Home.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="home-container">
+    <!-- 左侧区域菜单栏 -->
+    <div class="menu-list">
+      <a-menu
+        :default-selected-keys="['1']"
+        :default-open-keys="['sub1']"
+        mode="inline"
+        theme="dark"
+        :inline-collapsed="collapsed"
+      >
+        <a-menu-item key="1">
+          <a-icon type="pie-chart" />
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="desktop" />
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="inbox" />
+          <span>Option 3</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <span slot="title">
+            <a-icon type="mail" />
+            <span>Navigation One</span>
+          </span>
+          <a-menu-item key="5">Option 5</a-menu-item>
+          <a-menu-item key="6">Option 6</a-menu-item>
+          <a-menu-item key="7">Option 7</a-menu-item>
+          <a-menu-item key="8">Option 8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title">
+            <a-icon type="appstore" />
+            <span>Navigation Two</span>
+          </span>
+          <a-menu-item key="9">Option 9</a-menu-item>
+          <a-menu-item key="10">Option 10</a-menu-item>
+          <a-sub-menu key="sub3" title="Submenu">
+            <a-menu-item key="11">Option 11</a-menu-item>
+            <a-menu-item key="12">Option 12</a-menu-item>
+          </a-sub-menu>
+        </a-sub-menu>
+      </a-menu>
+    </div>
+
+    <!-- 右侧展示区 -->
+    <div :class="{ 'main-app': true, 'menu-unfold': collapsed }">
+      <div class="main-header">
+        <!-- 按钮 -->
+        <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
+          <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+        </a-button>
+        <!-- 面包屑 -->
+        <div class="breadcrumb">
+          <a-breadcrumb>
+            <a-breadcrumb-item>首页</a-breadcrumb-item>
+            <a-breadcrumb-item><a href="">统计</a></a-breadcrumb-item>
+          </a-breadcrumb>
+        </div>
+        <ul class="user-info">
+          <li>
+            欢迎法医
+            <a-icon type="down" />
+          </li>
+          <li>退出</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url('~@/assets/css/home.less');
+</style>
